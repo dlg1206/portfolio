2529d3c1e9e770d78fdcf0726bc6d0347207aed8
Derek Garcia <dcia811@gmail.com>
2026-01-28 17:52:19 -1000

    feat: Add fade in animation to info tiles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {TestComponent} from './pages/test/test.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test', component: TestComponent}
 ];
--- src/app/pages/home/components/info-tile/info-tile.component.ts
+++ src/app/pages/home/components/info-tile/info-tile.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {NgOptimizedImage, NgStyle} from '@angular/common';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 import {ImageItem} from '../../../../shared/models/image-item';
 import {SimpleButtonComponent} from '../../../../shared/components/simple-button/simple-button.component';
 
@@ -7,18 +7,38 @@ import {SimpleButtonComponent} from '../../../../shared/components/simple-button
   selector: 'app-info-tile',
   imports: [
     NgOptimizedImage,
-    SimpleButtonComponent,
-    NgStyle
+    SimpleButtonComponent
   ],
   templateUrl: './info-tile.component.html',
   styleUrl: './info-tile.component.css',
   standalone: true
 })
-export class InfoTileComponent {
+export class InfoTileComponent implements AfterViewInit {
   @Input() title!: string
   @Input() icon!: ImageItem
   @Input() content!: string
   @Input() routerLink!: string
   @Input() height: number = 60
   @Input() width: number = 60
+
+  // Grab a reference to the info-card DOM element
+  @ViewChild('card', {static: true}) card!: ElementRef;
+
+  ngAfterViewInit() {
+    // Create an IntersectionObserver to watch when the card enters the viewport
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // if onscreen, apply the fade in transition
+        if (entry.isIntersecting) {
+          this.card.nativeElement.classList.add('fade-in');
+          observer.unobserve(this.card.nativeElement); // animate once
+        }
+      },
+      {
+        threshold: 0.2, // triggers when 20% visible
+      }
+    );
+    // Stop observing after the animation runs once - prevent repeat fade in
+    observer.observe(this.card.nativeElement);
+  }
 }
