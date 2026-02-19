a4bfccd6ce27208dccecb74ab31f16e7aeb693c8
Derek Garcia <dcia811@gmail.com>
2026-02-18 16:45:46 -1000

    feat: Add publication block

---
+++ src/app/pages/publications/components/publication/publication.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {Publication} from '../../publications.component';
+import {PubButtonComponent} from '../pub-button/pub-button.component';
+
+
+@Component({
+  selector: 'app-publication',
+  imports: [
+    PubButtonComponent
+
+  ],
+  templateUrl: './publication.component.html',
+  styleUrl: './publication.component.css',
+})
+export class PublicationComponent {
+  // params for component
+  @Input() publication!: Publication;
+}
